b316ba4bcad34dc7b7ee1d7281bfede7e4c46a65
Rifa Safeer Shah <rifashah28@gmail.com>
2020-05-05 16:18:41 -0700

    Update TransferFund.java

--- atmMachine/src/atmMachine/TransferFund.java
+++ atmMachine/src/atmMachine/TransferFund.java
@@ -1,10 +1,32 @@
+/**
+ * Description:
+ * CECS 343 SPRING 2020
+ * ATM Machine:
+ * @author Rifa Safeer Shah and Nathaniel Monte De Ramos
+ * Date:
+ */
+
 package atmMachine;
 
+import java.util.Scanner;
+
 //transfers fund from one file to another
+
 public class TransferFund
+
 {
-	public static void transferFund() 
-	{
+
+	public static void transferFund() {
+		// TODO Auto-generated method stub
+		double transferAmount; //initial for transfer amount
+		double accBalance; //initial for the account balance
+		//boolean wrongAmount = true; //checks the amount
+		Scanner scan = new Scanner(System.in); //
+		System.out.println("Your balance is $" + Account.getBalance());
+		System.out.println("How much would you like to transfer?");
 		
 	}
+
+
+
 }
